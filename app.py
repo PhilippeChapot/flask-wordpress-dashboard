2714eb22397ae28f9ce89c8c96b7e87d3abba993
MB Pro de Philippe Chapot <mbprophilippechapot@MacBook-Pro-de-MB.local>
2025-02-22 15:43:42 +0100

    Ajout de requests dans requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+import requests
+import os
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__, template_folder='templates')
+
+# Paramètres WordPress
+WORDPRESS_API_URL = "https://podcastmagazine.fr/wp-json/wp/v2/posts"
+WORDPRESS_USERNAME = os.getenv("WORDPRESS_USERNAME", "PODCAST_MAGAZINE")
+WORDPRESS_PASSWORD = os.getenv("WORDPRESS_PASSWORD", "VOTRE_MOT_DE_PASSE")
+
+@app.route('/articles')
+def articles():
+    try:
+        response = requests.get(WORDPRESS_API_URL, auth=(WORDPRESS_USERNAME, WORDPRESS_PASSWORD))
+        articles = response.json()
+        return render_template("articles.html", articles=articles)
+    except Exception as e:
+        return f"Erreur lors de la récupération des articles: {str(e)}"
+
 
 @app.route('/')
 def home():
